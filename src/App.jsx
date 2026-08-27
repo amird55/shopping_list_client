29c4e6683f1548a92f81c686de3c51856cf091e2
amird55 <amir.dgn@outlook.com>
2026-08-27 20:25:49 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Homepage from "./_Features/Homepage/Homepage.jsx";
+
+function App() {
+
+  return (
+      <Homepage />
+  )
+}
+
+export default App
